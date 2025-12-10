80025febac98e7b1ee52d09cd6d106c0039e841a
Jaldert Rombouts <jaldert@pm.me>
2025-12-10 11:50:51 +0000

    Deal with multiple cuda installs

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 from setuptools import find_packages, setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
@@ -7,7 +8,29 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 requirements = ["torch"]
 
 
+def find_cuda_home():
+    """Find CUDA 12.x installation, required for torch 2.8.0+cu128."""
+    if "CUDA_HOME" in os.environ:
+        return os.environ["CUDA_HOME"]
+
+    # Search for CUDA 12.x in common locations
+    cuda_paths = glob.glob("/usr/local/cuda-12.*")
+    if cuda_paths:
+        # Return the highest version found
+        cuda_path = sorted(cuda_paths)[-1]
+        os.environ["CUDA_HOME"] = cuda_path
+        os.environ["CUDA_PATH"] = cuda_path
+        # Prepend CUDA bin to PATH
+        cuda_bin = os.path.join(cuda_path, "bin")
+        os.environ["PATH"] = f"{cuda_bin}:{os.environ.get('PATH', '')}"
+        return cuda_path
+
+    return None
+
+
 def get_extensions():
+    find_cuda_home()
+
     srcs = ["cc_torch/connected_components.cu"]
     extra_compile_args = {
         "cxx": [],
